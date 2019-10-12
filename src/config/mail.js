87e68619f9e730d9c01f22a087e7d628a0428731
Raniere Costa <raniere_@live.com>
2019-10-12 15:28:00 -0300

    Criado cancelamento de   agendamento com envio de email ao provider

--- src/config/mail.js
+++ src/config/mail.js
@@ -4,7 +4,7 @@ export default {
   secure: false,
   auth: {
     user: '3de904600e7b94',
-    pass: '3de904600e7b94',
+    pass: '65d5251514a6a8',
   },
   default: {
     from: 'Team GoBarber <noreply@gobarber.com>',
