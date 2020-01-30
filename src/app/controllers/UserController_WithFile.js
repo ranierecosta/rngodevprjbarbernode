dbcd3e0a7011dd9ab6c7fd57d8b701c1d1bcd70e
Raniere Costa <raniere_@live.com>
2020-01-30 16:58:21 -0300

    Ajuste do endpoint do usuário para prevenir quanto ao avatar do usuário

--- src/app/controllers/UserController_WithFile.js
+++ src/app/controllers/UserController_WithFile.js
@@ -41,12 +41,32 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
 
     if (email !== user.email) {
-      const userExists = await User.findOne({ where: { email } });
+      const userExists = await User.findOne({
+        where: { email },
+      });
 
       if (userExists) {
         return res.status(400).json({ error: 'User already exists.' });
@@ -59,7 +79,7 @@ class UserController {
 
     await user.update(req.body);
 
-    const { id, name, avatar } = await User.findByPk(req.userId, {
+    const { id, name, avatar } = await user.findByPk(req.userId, {
       include: [
         {
           model: File,
